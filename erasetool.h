3ccb563614e7fc2ed4e67a3d88778f725b93bf87
Bogdan Pryadko <bogdanpryadko1@gmail.com>
2025-03-28 12:20:15 +0300

    [Core] update erasetool class (header)

--- erasetool.h
+++ erasetool.h
@@ -1,4 +1,21 @@
-#ifndef ERASETOOL_H
-#define ERASETOOL_H
+#ifndef ERASERTOOL_H
+#define ERASERTOOL_H
 
-#endif // ERASETOOL_H
+#include "basetool.h"
+
+class EraserTool : public BaseTool
+{
+    Q_OBJECT
+public:
+    explicit EraserTool(QObject *parent = nullptr);
+
+    void onMousePress(const QPoint &point, Canvas *canvas) override;
+    void onMouseMove(const QPoint &point, Canvas *canvas) override;
+    void onMouseRelease(const QPoint &point, Canvas *canvas) override;
+
+private:
+    QPoint m_lastPoint;
+    bool m_drawing;
+};
+
+#endif
