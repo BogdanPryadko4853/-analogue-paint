b7d8f02a22ab56b7368a671dec1bfa6f48b2e9bf
Bogdan Pryadko <bogdanpryadko1@gmail.com>
2025-03-28 12:08:55 +0300

    [Core] Update Canvas API (header)

--- basetool.h
+++ basetool.h
@@ -1,4 +1,29 @@
 #ifndef BASETOOL_H
 #define BASETOOL_H
 
+#include <QObject>
+#include <QPoint>
+#include <QPen>
+
+class Canvas;
+
+class BaseTool : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BaseTool(QObject *parent = nullptr);
+    virtual ~BaseTool() = default;
+
+    virtual void onMousePress(const QPoint &point, Canvas *canvas) = 0;
+    virtual void onMouseMove(const QPoint &point, Canvas *canvas) = 0;
+    virtual void onMouseRelease(const QPoint &point, Canvas *canvas) = 0;
+
+    void setPen(const QPen &pen);
+    const QPen& pen() const;
+
+protected:
+    QPen m_pen;
+    QPoint m_lastPoint;
+};
+
 #endif // BASETOOL_H
